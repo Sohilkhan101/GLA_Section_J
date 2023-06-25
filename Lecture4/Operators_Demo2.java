a8bbe4917e7547a7bf0d80436db52ed712fdda59
Sohil khan <71373974+Sohilkhan101@users.noreply.github.com>
2023-06-25 23:47:02 +0530

    Basics Qps

---
+++ Lecture4/Operators_Demo2.java
@@ -0,0 +1,81 @@
+package Lecture4;
+
+public class Operators_Demo2 {
+	public static void main(String[] args) {
+
+		// Arithmetic operators
+		
+//		int a = 4;
+//		int b = 6;
+//
+//		System.out.println(a + b);
+//		System.out.println(a - b);
+//		System.out.println(a * b);
+//		System.out.println(a / b);
+//		System.out.println(a % b);
+		
+		// Assignment operators
+
+//		int x = 13;
+//		System.out.println(x+=2);
+//		System.out.println(x-=2);
+//		System.out.println(x*=2);
+//		System.out.println(x/=2);
+//		System.out.println(x%=2);
+		
+
+		// Logical operators
+
+		// AND(&&)
+//		int a=10;
+//		int b=12;
+//		int c=14;
+		
+//		System.out.println((a>b) && (b>c));
+//		
+//		System.out.println((a<b) && (b>c));
+//		
+//		System.out.println((a>b) && (b<c));
+//		
+//		System.out.println((a<b) && (b<c));
+				
+//		System.out.println(false && false);
+//		System.out.println(true && false);
+//		System.out.println(false && true);
+//		System.out.println(true && true);
+//
+		// OR(||)
+//		
+//		System.out.println((a>b) && (b>c));
+//		
+//		System.out.println((a<b) && (b>c));
+//		
+//		System.out.println((a>b) && (b<c));
+//		
+//		System.out.println((a<b) && (b<c));
+	
+//		System.out.println(false || false);
+//		System.out.println(true  || false);
+//		System.out.println(false || true);
+//		System.out.println(true  || true);
+		
+		// NOT(!)
+		
+//		boolean x= true;
+//		System.out.println(!x);
+		
+
+		// Comparison operators
+		
+//		int x = 10;
+//		int y = 12;
+//		System.out.println(x < y);
+//		System.out.println(x > y);
+//		System.out.println(x <= y);
+//		System.out.println(x >= y);
+//		System.out.println(x != y);
+//		System.out.println(x == y);
+
+		
+	}
+}
